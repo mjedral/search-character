3ff137d9be7d8a8ad259be4c6653ed42b81af6d3
mjedral <miazga198@o2.pl>
2023-02-02 21:55:09 +0100

    chore: apollo client

---
+++ apolloClient.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'https://rickandmortyapi.com/graphql',
+  }),
+});
