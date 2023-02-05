39a0477f3278436b111a19f6fa68d9f593e168f5
mjedral <miazga198@o2.pl>
2023-02-05 01:12:52 +0100

    fix: added ApolloProvider and GlobalStyles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client';
+import type { AppProps } from 'next/app';
+import { client } from '../apolloClient';
+import { GlobalStyles } from '../styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
